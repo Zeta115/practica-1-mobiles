c93397f6d53cc5ad5e5835cc6da2683d310ecd86
Zeta115 <polpallares3@gmail.com>
2022-11-04 19:37:05 +0100

    bueno almenos parece una pantalla

--- practica1/lib/main.dart
+++ practica1/lib/main.dart
@@ -11,13 +11,139 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Practica 1 PolPallaresMur",
       theme: ThemeData.dark(),
-      home: const Pantalla(),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 0,
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+            ),
+            Icon(
+              Icons.heart_broken,
+              color: Colors.red,
+            ),
+            Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.blue,
+                )),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 400,
+                    width: 500,
+                    color: Colors.white,
+                  ),
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Mount of Brecken",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Colors.black),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.calendar_month,
+                            color: Colors.black,
+                          ),
+                          Text(
+                            "3 days/4 nights",
+                            style: TextStyle(fontSize: 15, color: Colors.black),
+                          ),
+                          Padding(padding: EdgeInsets.all(40.0)),
+                          Align(),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                              margin: const EdgeInsets.all(30.0),
+                              decoration: const BoxDecoration(
+                                  color: Colors.red, shape: BoxShape.circle)),
+                          Container(
+                              margin: const EdgeInsets.all(30.0),
+                              decoration: const BoxDecoration(
+                                  color: Colors.red, shape: BoxShape.circle)),
+                          Container(
+                              margin: const EdgeInsets.all(30.0),
+                              decoration: const BoxDecoration(
+                                  color: Colors.red, shape: BoxShape.circle)),
+                          Container(
+                              margin: const EdgeInsets.all(30.0),
+                              decoration: const BoxDecoration(
+                                  color: Colors.red, shape: BoxShape.circle)),
+                        ],
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "About",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen bookLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 10,
+                            color: Colors.black),
+                      ),
+                      Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            width: 200,
+                            height: 50,
+                            color: Colors.orange,
+                            padding: EdgeInsets.all(30.0),
+                          )),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
+/*
 class Pantalla extends StatelessWidget {
   const Pantalla({Key? key}) : super(key: key);
 
@@ -47,8 +173,15 @@ class Pantalla extends StatelessWidget {
           Align(
               alignment: Alignment.topLeft,
               child: Text(
-                "Titular",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                "Mount of Brecken",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              )),
+          Expanded(child: Wcirculos()),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                style: TextStyle(fontWeight: FontWeight.normal, fontSize: 10),
               )),
         ]),
         const Align(
@@ -74,3 +207,4 @@ class Wcirculos extends StatelessWidget {
     ]));
   }
 }
+*/
